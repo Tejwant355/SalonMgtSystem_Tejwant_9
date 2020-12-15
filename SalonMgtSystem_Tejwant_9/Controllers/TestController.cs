43e6653cb7388c81d1102becc317b96b72d7f13b
randhawamehak41@gmail.com <randhawamehak41@gmail.com>
2020-12-15 07:50:33 +0530

    Add project files.

---
+++ SalonMgtSystem_Tejwant_9/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalonMgtSystem_Tejwant_9.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SalonMgtSystem_Tejwant_9/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalonMgtSystem_Tejwant_9.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
